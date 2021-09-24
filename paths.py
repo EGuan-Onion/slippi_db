d26444de44c018a66b2690de5728ad14600f2262
Eric Guan <onionchowder@gmail.com>
2021-09-24 16:23:51 -0700

    Update paths.py

--- paths.py
+++ paths.py
@@ -1,23 +1,8 @@
-# DIR_PATH = "/Volumes/T7/slippi_db/"
-# REPLAY_DIR_PATH = DIR_PATH + "replays/"
-# RAW_DB_PATH = DIR_PATH + "db/raw.db"
-# PARSE_QUEUE_PATH = DIR_PATH + "parse_queue.json"
-# SQL_OUTPUT_DIR_PATH = DIR_PATH + "sql_output/"
-# SQL_DIR_PATH = "./sql/"
-
-# REPO_DIR_PATH = "/Users/eguan/slippi_db/"
-# DIM_TABLE_DIR_PATH = REPO_DIR_PATH + "dim_tables/"
-
-
-# TEST_DIR_PATH = "/Users/eguan/slippi_db/test/"
-# TEST_REPLAY_DIR_PATH = TEST_DIR_PATH + "replays/"
-# TEST_RAW_DB_PATH = TEST_DIR_PATH + "db/raw_test.db"
-# TEST_PARSE_QUEUE_PATH = TEST_DIR_PATH + "parse_queue_test.json"
-# TEST_SQL_OUTPUT_DIR_PATH = TEST_DIR_PATH + "sql_output/"
-# TEST_SQL_DIR_PATH = SQL_DIR_PATH + "test/"
+#filepaths
+#mode in (test, local, production)
 
 class Paths():
-	def __init__(self, mode="test"):
+	def __init__(self, mode):
 		self.REPO_DIR_PATH = "/Users/eguan/slippi_db/"
 		self.STATIC_TABLE_DIR_PATH = self.REPO_DIR_PATH + "static_tables/"
 		self.SQL_DIR_PATH = self.REPO_DIR_PATH + "sql/"
